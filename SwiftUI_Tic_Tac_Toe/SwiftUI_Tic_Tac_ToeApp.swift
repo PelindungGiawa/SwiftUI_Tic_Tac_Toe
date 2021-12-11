a340e060cca0473e4c636265e995583eca9a23f4
PelindungGiawa <61076880+PelindungGiawa@users.noreply.github.com>
2021-12-11 14:50:48 +0700

    Initial Commit

---
+++ SwiftUI_Tic_Tac_Toe/SwiftUI_Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Tic_Tac_ToeApp.swift
+//  SwiftUI_Tic_Tac_Toe
+//
+//  Created by Pelindung Giawa on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
